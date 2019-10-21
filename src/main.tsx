d955ba46c2a0c640d1145ff0a5e3897368bd510c
Matthew Howard <matthew@hattmo.com>
2019-10-21 10:26:29 -0500

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import App from "./app.tsx";
+
+const root = document.createElement("div");
+document.body.appendChild(root);
+
+ReactDom.render(
+  <App/>,
+  root,
+);
