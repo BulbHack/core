0435cb45a0670781b10c1f56c61ed35d82f56855
Hattmo <matthew@hattmo.com>
2020-09-15 10:06:47 -0500

    modifier hooks optional

--- src/lib/modifier.ts
+++ src/lib/modifier.ts
@@ -1,16 +1,25 @@
 import { Entity, IterateFunction } from "./app";
 
-export const createMod = <R, A>(
-  initialState: A,
-  beforeIter: IterateFunction<R & A>,
-  afterIter: IterateFunction<R & A>
-) => {
+export const createMod = <R, A>({
+  initialState,
+  beforeIter,
+  afterIter,
+}: {
+  initialState: A;
+  beforeIter?: IterateFunction<R & A>;
+  afterIter?: IterateFunction<R & A>;
+}) => {
   return <T>(target: Entity<R & T>): Entity<R & T & A> => {
     const [targetInitialState, targetIterator] = target;
     const comboIterator: IterateFunction<R & T & A> = (args) => {
-      args.state = { ...args.state, ...beforeIter(args) };
+      if (beforeIter !== undefined) {
+        args.state = { ...args.state, ...beforeIter(args) };
+      }
       args.state = { ...args.state, ...targetIterator(args) };
-      return { ...args.state, ...afterIter(args) };
+      if (afterIter !== undefined) {
+        args.state = { ...args.state, ...afterIter(args) };
+      }
+      return args.state;
     };
     return [{ ...targetInitialState, ...initialState }, comboIterator];
   };
--- test/testRun/modifiers/falls.ts
+++ test/testRun/modifiers/falls.ts
@@ -3,11 +3,12 @@ import { IPositionState } from "../states/states";
 
 const falls = <T>(target: Entity<IPositionState & T>) => {
   const fallSpeed = Math.floor(Math.random() * 2 + 8);
-  const mod = createMod<IPositionState, { isFalling: boolean }>(
-    { isFalling: true },
-    (param) => {
+  const mod = createMod<IPositionState, { isFalling: boolean }>({
+    initialState: { isFalling: true },
+    beforeIter: (param) => {
       let falling = true;
       if (param.state.y > 2000) {
+        console.log("died");
         param.die();
         falling = false;
       }
@@ -17,10 +18,8 @@ const falls = <T>(target: Entity<IPositionState & T>) => {
         isFalling: falling,
       };
     },
-    (param) => param.state
-  );
+  });
   return mod(target);
 };
-
 
 export default falls;
